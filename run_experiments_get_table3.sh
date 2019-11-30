b970a47c6558e6cbea7aa9fb0326c144f99b53d3
Hongyuan Liu <hliu08@email.wm.edu>
2019-11-30 02:18:57 -0500

    run for table 3

--- run_experiments_get_table3.sh
+++ run_experiments_get_table3.sh
@@ -1,5 +1,14 @@
 cd ${GPUNFA_ROOT}
-mkdir exp_table3 && cd exp_table3
+
+
+
+
+if [ ! -d "gpunfa_benchmarks" ]; then
+    mkdir exp_table3 && cd exp_table3
+else
+    cd exp_table3
+fi
+
 cp ../gpunfa_code/scripts/configs/* .
 
 echo "Running Experiments... This will take several hours. "
@@ -8,16 +17,17 @@ python ../gpunfa_code/scripts/launch_exps.py -b app_spec -f exec_config_table3 -
 echo "Experiments finished. "
 
 
-echo "Collecting experiment raw data."
-python ../gpunfa_code/scripts/collect_results.py -b app_spec -f exec_config_table3 
+if [ $? -eq 0 ]; then
+    echo "Collecting experiment raw data."
+    python ../gpunfa_code/scripts/collect_results.py -b app_spec -f exec_config_table3 
 
-
-echo "Generate the Table 3 from the raw data. "
-python ../gpunfa_code/scripts/plotting/abs_throughput_table.py
-
-
-echo "You may find a csv file in the folder, which should be similar to Table 3 in our paper. "
-
+    echo "Generate the Table 3 from the raw data. "
+    python ../gpunfa_code/scripts/plotting/abs_throughput_table.py
+    echo "You may find a csv file in the folder, which should be similar to Table 3 in our paper. "
+else
+    echo "Experiments terminate abnormally. "
+    exit 1
+fi
 
 
 
